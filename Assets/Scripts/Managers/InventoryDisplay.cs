6e4e93e63c19891ae1506f85da22f2321c399700
Cory Johns <cory@triplevisiongroup.com>
2025-05-10 17:53:52 -0700

    Refactored Inventory System:
    - TODO trigger InventoryDisplay . UpdateDisplay from an event
    - TODO setup trade manager to swap station inventories
    - Refactored inventory display to use new inventory system
    - refactored TradeManager to use new inventory system
    - new inventory system is a significant upgrade from the original. All item prefabs are referenced in a single item manager prefab, ui elements are now instanced from a single prefab and items no longer need 2 prefabs (item and ui). The items themselves are still scriptable objects, but the new sprite field enables the above functionality. The item manager prefab has been pre-configured, so it just needs to be dragged into scenes that require it. All scripts that require the item manager look it up in the scene hierarchy and don't require a reference to be applied.

--- Assets/Scripts/Managers/InventoryDisplay.cs
+++ Assets/Scripts/Managers/InventoryDisplay.cs
@@ -1,93 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 using TMPro;
+using System;
+using static UnityEditor.Progress;
 
+/**
+ * Inventory Display handles managing the user interface of a single inventory.
+ * It is placed on the window the items are placed in.
+ */
 public class InventoryDisplay : MonoBehaviour
 {
-    public InventoryObject inventory;
-    public GameObject item_ui_prefab;
+    // this is the inventory managed by this Inventory Display
+    [SerializeField] private InventoryObject inventory;
+    // The item manager that can translate the ItemType enum to the associated ItemObject.
+    private ItemManager item_manager;
     public bool show_price = false;
-    Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool is_player_inv = false;
+    // internal dictionary of all items in the display, including disabled ones
+    private Dictionary<ItemType, GameObject> itemsDisplayed = new();
+
     void Start()
     {
+        // lookup the item manager
+        item_manager = GameObject.Find("ItemManager").GetComponent<ItemManager>();
+        //Debug.Log("Item manager :: " +  item_manager);
+        // make sure the inventory is initialized
+        inventory.InitInventory();
+        // set up the display with all inventory objects, disable the ones with no items
         CreateDisplay();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    public void UpdateDisplay(ItemType item)
     {
-        UpdateDisplay();
-    }
-
-    public void UpdateDisplay()
-    {
-        int i=0;
-        foreach (InventorySlot slot in inventory.container)
+        // updates a single item in the display, call from an event
+        if (itemsDisplayed.ContainsKey(item))
         {
-            if (itemsDisplayed.ContainsKey(slot))
-            {
-                UpdateItemText(itemsDisplayed[slot], slot);
-            }
-            else
-            {
-                AddItemUI(slot);
-            }
-            i++;
+            UpdateItemText(item, itemsDisplayed[item]);
+        }
+        else
+        {
+            AddItemUI(item);
         }
     }
 
     public void CreateDisplay()
     {
-        int i=0;
-        foreach (InventorySlot slot in inventory.container)
+        // add an entry for each item
+        foreach (ItemType item in Enum.GetValues(typeof(ItemType)))
         {
-            AddItemUI(slot);
-            i++;
+            AddItemUI(item);
         }
     }
-
-    public void AddItemUI(InventorySlot _slot)
+    public void UpdateItemText(ItemType item, GameObject game_object)
     {
-        // skip null slots, items, and prefabs
-        if (_slot.item != null)
+        if (inventory.GetItemAmount(item) == 0) // no item
         {
-            // add an item ui to the menu
-            var obj = Instantiate(item_ui_prefab, transform);
-            // update item icon
-            Image[] imageResults = GetComponentsInChildren<Image>();
-            // find the icon
-            foreach (Image img in imageResults)
-            {
-                if (img.name == "ItemIcon")
-                {
-                    img.sprite = _slot.item.icon;
-                    break;
-                }
-            }
-            // update the text
-            UpdateItemText(obj, _slot);
-            itemsDisplayed.Add(_slot, obj);
-        }
-    }
-
-    public void UpdateItemText(GameObject _item, InventorySlot _slot)
-    {
-        if (_slot.amount == 0) {
             // don't display this slot
-            _item.SetActive(false);
+            game_object.SetActive(false);
         }
-        else if (!_item.activeSelf)
+        else if (!game_object.activeSelf) // icon is disabled, already check that it isn't 0
         {
             // turn item back on if amount isn't 0
-            _item.SetActive(true);
+            game_object.SetActive(true);
         }
-        else
+        else // amount != 0 and icon is active
         {
             // get all child Components of type TextMeshProUGUI, these are the elements that will be updated
-            TextMeshProUGUI[] components_list = _item.GetComponentsInChildren<TextMeshProUGUI>();
+            TextMeshProUGUI[] components_list = game_object.GetComponentsInChildren<TextMeshProUGUI>();
 
             // check each result, update the amount text and the item name
             foreach (TextMeshProUGUI ugui in components_list)
@@ -95,19 +76,42 @@ public class InventoryDisplay : MonoBehaviour
                 // set the amount element
                 if (ugui.name == "ItemAmountText")
                 {
-                    ugui.text = _slot.amount.ToString("n0");
+                    ugui.text = inventory.GetItemAmount(item).ToString("n0");
                 }
                 // set the name element
                 else if (ugui.name == "ItemNameText")
                 {
-                    ugui.text = _slot.item.item_name;
+                    ugui.text = item_manager.GetItem(item).item_name;
                 }
                 // set the price element
                 else if (show_price && ugui.name == "ItemPriceText")
                 {
-                    ugui.text = _slot.item.item_value.ToString("C2");
+                    ugui.text = item_manager.GetItem(item).item_value.ToString("C2");
                 }
             }
         }
     }
-}
+
+    public void AddItemUI(ItemType item)
+    {
+        // add an item ui to the menu
+        var obj = Instantiate(item_manager.item_ui_prefab, transform);
+        // update item icon
+        Image[] imageResults = obj.GetComponentsInChildren<Image>();
+        // find the icon
+        foreach (Image img in imageResults)
+        {
+            if (img.name == "ItemIcon")
+            {
+                // update the icon
+                Debug.Log("adding item icon: " + item + " " + item_manager.GetItem(item));
+                img.sprite = item_manager.GetItem(item).icon;
+                break;
+            }
+        }
+        // update the text
+        UpdateItemText(item, obj);
+        itemsDisplayed.Add(item, obj);
+        }
+    }
+    
--- Assets/Scripts/Managers/TradeManager.cs
+++ Assets/Scripts/Managers/TradeManager.cs
@@ -5,103 +5,113 @@ using TMPro;
 
 
 /**
- *
  * Manages: station inventories, trading, item production, supply and demand calculations.
- *
  */
 public class TradeManager : MonoBehaviour
 {
-    public InventoryObject playerInventory;
-    public List<Station> stations = new List<Station>();
-    public InventoryObject stationTESTInventory;
-    public GameObject playerTradeArea;
-    public GameObject stationTradeArea;
-    public TextMeshProUGUI playerCreditsText;
-    public TextMeshProUGUI stationCreditsText;
-    public TMP_InputField quantityValueText;
-
+    [SerializeField] private InventoryObject playerInventory;
+    [SerializeField] private List<InventoryObject> tradingPosts = new List<InventoryObject>();
+    private ItemManager itemManager;
+    private GameObject playerTradeArea;
+    private GameObject stationTradeArea;
+    private TextMeshProUGUI playerCreditsText;
+    private TextMeshProUGUI stationCreditsText;
+    private TMP_InputField quantityValueText;
+    private bool is_initialized = false;
     private int currentStation = 0;
 
-    void Awake()
+    private void Awake()
     {
-        stations.Add(new Station(stationTESTInventory));
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        // lookup all components needed by the trade manager
+        // lookup item manager
+        itemManager = GameObject.Find("ItemManager").GetComponent<ItemManager>();
+        // most are children of the Trade Menu
+        GameObject trade_menu = GameObject.Find("Trade Menu");
+        // Debug.Log("trade_menu=" + trade_menu.name);
+        // lookup player trade area. It is a child of trade_menu, so use transform.Find
+        playerTradeArea = trade_menu.transform.Find("Viewport").Find("PlayerInventoryContent").gameObject;
+        // Debug.Log("player trade area=" + playerTradeArea.name);
+        // lookup station trade area
+        stationTradeArea = trade_menu.transform.Find("Viewport").Find("StationInventoryContent").gameObject;
+        // Debug.Log("station trade area=" + stationTradeArea.name);
+        // lookup player credits text area
+        playerCreditsText = trade_menu.transform.Find("CreditsPanel").Find("PlayerCreditsValueText").gameObject.GetComponent<TextMeshProUGUI>();
+        Debug.Log("player credits text=" + playerCreditsText.name);
+        // lookup station credits text area
+        stationCreditsText = trade_menu.transform.Find("CreditsPanel").Find("StationCreditsValueText").gameObject.GetComponent<TextMeshProUGUI>();
+        // Debug.Log("station credits text=" + stationCreditsText.name);
+        // lookup quantity text area
+        quantityValueText = trade_menu.transform.Find("QuantityUI").Find("QuantityInputField").gameObject.GetComponent<TMP_InputField>();
+        // Debug.Log("quantity value text=" + quantityValueText.name);
+        is_initialized = true;
     }
 
     void OnEnable ()
     {
+        if (!is_initialized)
+        {
+            Initialize();
+        }
         // update player credits text
         playerCreditsText.text = playerInventory.credits.ToString("c2");
         // update station credits text
-        stationCreditsText.text = stations[currentStation].stationInventory.credits.ToString("c2");
+        stationCreditsText.text = tradingPosts[currentStation].credits.ToString("c2");
     }
 
-    float GetItemCost(ItemObject _item)
+    float GetItemCost(ItemType item)
     {
         //TODO add supply and demand calculations
-        return _item.item_value;
+        return itemManager.GetItem(item).item_value;
     }
 
-    public void ClickedOn(ItemType _item_type, InventoryObject _inventory_containing)
+    /**
+     * Process a click event from an item in an inventory.
+     */
+    public void ClickedOn(ItemType item_type, bool is_player_inv)
     {
-        // get the ItemObject connected with the ItemType
-        ItemObject item_clicked = _inventory_containing.FindByType(_item_type);
-        // check for null item
-        if (item_clicked)
+        // get the amount
+        int quantity = int.Parse(quantityValueText.text);
+        // determine the other inventory
+        if (is_player_inv)
         {
-            // get the amount
-            int quantitiy = int.Parse(quantityValueText.text);
-            // determine the other inventory
-            if (_inventory_containing == playerInventory)
-            {
-                // other inventory is a station inventory
-                Debug.Log("player inventory clicked, quantitiy = " + quantitiy);
-                TradeItems(item_clicked, _inventory_containing, stations[currentStation].stationInventory, quantitiy);
-            }
-            else
-            {
-                // other inventory is the player inventory
-                Debug.Log("station inventory clicked, quantitiy = " + quantitiy);
-                TradeItems(item_clicked, _inventory_containing, playerInventory, quantitiy);
-            }
+            // other inventory is a station inventory
+            Debug.Log("player inventory clicked, quantity = " + quantity);
+            TradeItems(item_type, playerInventory, tradingPosts[currentStation], quantity);
         }
         else
         {
-            Debug.Log("Clicked Item was not found in inventory!");
+            // other inventory is the player inventory
+            Debug.Log("station inventory clicked, quantity = " + quantity);
+            TradeItems(item_type, tradingPosts[currentStation], playerInventory, quantity);
         }
     }
 
-    public void TradeItems(ItemObject _item_from, InventoryObject _inventory_from, InventoryObject _inventory_to, int amount)
+    public void TradeItems(ItemType item_from, InventoryObject inventory_from, InventoryObject inventory_to, int amount)
     {
-        float credits_buyer = _inventory_to.credits;
-        float unit_cost = GetItemCost(_item_from);
+        float credits_buyer = inventory_to.credits;
+        float unit_cost = GetItemCost(item_from);
         float full_cost = amount * unit_cost;
         // allow transaction if full cost can be paid for
-        if (full_cost <= _inventory_to.credits)
+        if (full_cost <= inventory_to.credits)
         {
             // remove items from seller inventory
-            int sold_quantity = _inventory_from.RemoveItem(_item_from, amount);
+            int sold_quantity = inventory_from.RemoveItem(item_from, amount);
             // add item to buyer inventory
-            _inventory_to.AddItem(_item_from, sold_quantity);
+            inventory_to.AddItem(item_from, sold_quantity);
             // add credits to seller inventory
-            _inventory_from.credits += (sold_quantity * unit_cost);
+            inventory_from.credits += (sold_quantity * unit_cost);
             // remove credits form buyer inventory
-            _inventory_to.credits -= (sold_quantity * unit_cost);
+            inventory_to.credits -= (sold_quantity * unit_cost);
             // update ui
             // update player credits text
             playerCreditsText.text = playerInventory.credits.ToString("c2");
             // update station credits text
-            stationCreditsText.text = stations[currentStation].stationInventory.credits.ToString("c2");
+            stationCreditsText.text = tradingPosts[currentStation].credits.ToString("c2");
         }
-    }
-}
-
-
-public class Station
-{
-    public InventoryObject stationInventory;
-
-    public Station(InventoryObject inventory)
-    {
-        stationInventory = inventory;
     }
 }
