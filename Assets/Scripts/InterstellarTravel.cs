2447b3085921267a1a9538348c91e7c5b1d77760
Cory Johns <cory@triplevisiongroup.com>
2025-05-27 19:57:52 -0700

    Branches merged and work partially combined:
    - interstellar markers can transition between systems
    - both branches merged with 0 data loss
    - camera bugs out when a new scene is loaded, likely because I didn't hook it up right
    - scene transitions move to wrong scene, likely because the wrong index is generated when reading the string

--- Assets/Scripts/InterstellarTravel.cs
+++ Assets/Scripts/InterstellarTravel.cs
@@ -1,7 +1,15 @@
+using System;
 using UnityEngine;
 
 public class InterstellarTravel : MonoBehaviour
 {
+    private SaveManagerInterface saveManager;
+    [Header("Default settings")]
+    [SerializeField] private string global_scripts = "GlobalScripts";
+    private void Start()
+    {
+        saveManager = GameObject.Find(global_scripts).GetComponent<SaveManagerInterface>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Ship")) return;
@@ -11,5 +19,12 @@ public class InterstellarTravel : MonoBehaviour
             ? string.Join(" ", words, 0, words.Length - 1)
             : gameObject.name;
         Debug.Log($"Entering interstellar tunnel to {systemName}");
+        // load other scene
+        int index = 0;
+        if (Int32.TryParse(words[1], out index))
+        {
+            // index should be the number of the zone, like 3 in the case of name="Zone 3".
+            saveManager.SceneTransitionByIndex(index);
+        }
     }
 }
