7f77fd58471eb7dcbf3ba20384ee9932bc583884
Cory Johns <cory@triplevisiongroup.com>
2025-05-27 22:42:18 -0700

    Changed Game Title

--- Assets/Scripts/Managers/Inventory.cs
+++ Assets/Scripts/Managers/Inventory.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public class Inventory : MonoBehaviour
 {
